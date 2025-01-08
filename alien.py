9b3be986a931c9d38951c1c5e167e521b4b4bea9
Kym Derriman <138180384+spookyusers@users.noreply.github.com>
2025-01-08 13:16:54 -0500

    Method to update alien and alien fleet movement.

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,3 +19,8 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+    
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
