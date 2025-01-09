771cba411f3d2512ef56890c0dc3f0c2ffc08410
Kym Derriman <138180384+spookyusers@users.noreply.github.com>
2025-01-08 20:33:44 -0500

    Deactivate play button during gameplay.

--- alien_invasion.py
+++ alien_invasion.py
@@ -183,18 +183,20 @@ class AlienInvasion:
     
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
-            # Reset game statistics
-            self.stats.reset_stats()
-            self.game_active = True
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            if self.play_button.rect.collidepoint(mouse_pos):
+                # Reset game statistics
+                self.stats.reset_stats()
+                self.game_active = True
 
-            # Get rid of any remaining bullets and aliens.
-            self.bullets.empty()
-            self.aliens.empty()
+                # Get rid of any remaining bullets and aliens.
+                self.bullets.empty()
+                self.aliens.empty()
 
-            # Create a new fleet and center the ship.
-            self._create_fleet()
-            self.ship.center_ship()
+                # Create a new fleet and center the ship.
+                self._create_fleet()
+                self.ship.center_ship()
                 
     def _check_keydown_events(self, event):
         """Respond to key presses."""
