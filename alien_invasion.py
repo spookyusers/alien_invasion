b18581aa5e9c1bb9fec9e7d9b9f4b927af446103
Kym Derriman <138180384+spookyusers@users.noreply.github.com>
2025-01-08 20:31:09 -0500

    Reset everything when play button pressed.

--- alien_invasion.py
+++ alien_invasion.py
@@ -184,7 +184,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset game statistics
+            self.stats.reset_stats()
             self.game_active = True
+
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
                 
     def _check_keydown_events(self, event):
         """Respond to key presses."""
